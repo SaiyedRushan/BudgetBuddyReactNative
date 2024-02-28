01a741e8fd93a8e56acfbbd0ea08d5236b7eaec4
SaiyedRushan <rushan52@gmail.com>
2024-02-28 15:11:40 -0500

    fetch data from db and display transaction summary

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,12 +1,111 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { ScrollView, StyleSheet, Text, TextStyle, View } from "react-native";
+import { useSQLiteContext } from "expo-sqlite/next";
+import Card from "../components/ui/Card";
+import { useEffect, useState } from "react";
+import { Category, Transaction, TransactionsByMonth } from "../types";
 
 const Home = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [transactionsByMonth, setTransactionsByMonth] = useState<TransactionsByMonth>({
+    totalExpenses: 0,
+    totalIncome: 0,
+  });
+
+  const db = useSQLiteContext();
+  useEffect(() => {
+    db.withTransactionAsync(async () => {
+      await getData();
+    });
+  }, [db]);
+
+  async function getData() {
+    const result = await db.getAllAsync<Transaction>(`SELECT * FROM Transactions ORDER BY date DESC;`);
+    setTransactions(result);
+
+    const categoriesResult = await db.getAllAsync<Category>(`SELECT * FROM Categories;`);
+    setCategories(categoriesResult);
+
+    const now = new Date();
+    const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+    const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+    endOfMonth.setMilliseconds(endOfMonth.getMilliseconds() - 1);
+
+    // Convert to Unix timestamps (seconds)
+    const startOfMonthTimestamp = Math.floor(startOfMonth.getTime() / 1000);
+    const endOfMonthTimestamp = Math.floor(endOfMonth.getTime() / 1000);
+
+    const transactionsByMonth = await db.getAllAsync<TransactionsByMonth>(
+      `
+      SELECT
+        COALESCE(SUM(CASE WHEN type = 'Expense' THEN amount ELSE 0 END), 0) AS totalExpenses,
+        COALESCE(SUM(CASE WHEN type = 'Income' THEN amount ELSE 0 END), 0) AS totalIncome
+      FROM Transactions
+      WHERE date >= ? AND date <= ?;
+    `,
+      [startOfMonthTimestamp, endOfMonthTimestamp]
+    );
+    setTransactionsByMonth(transactionsByMonth[0]);
+  }
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView contentContainerStyle={{ padding: 15, paddingVertical: 170 }}>
+      <TransactionSummary totalExpenses={transactionsByMonth.totalExpenses} totalIncome={transactionsByMonth.totalIncome} />
+    </ScrollView>
   );
 };
+
+function TransactionSummary({ totalIncome, totalExpenses }: TransactionsByMonth) {
+  const savings = totalIncome - totalExpenses;
+  const readablePeriod = new Date().toLocaleDateString("default", {
+    month: "long",
+    year: "numeric",
+  });
+
+  // Function to determine the style based on the value (positive or negative)
+  const getMoneyTextStyle = (value: number): TextStyle => ({
+    fontWeight: "bold",
+    color: value < 0 ? "#ff4500" : "#2e8b57", // Red for negative, custom green for positive
+  });
+
+  // Helper function to format monetary values
+  const formatMoney = (value: number) => {
+    const absValue = Math.abs(value).toFixed(2);
+    return `${value < 0 ? "-" : ""}$${absValue}`;
+  };
+
+  return (
+    <Card style={styles.container}>
+      <Text style={styles.periodTitle}>Summary for {readablePeriod}</Text>
+      <Text style={styles.summaryText}>
+        Income: <Text style={getMoneyTextStyle(totalIncome)}>{formatMoney(totalIncome)}</Text>
+      </Text>
+      <Text style={styles.summaryText}>
+        Total Expenses: <Text style={getMoneyTextStyle(totalExpenses)}>{formatMoney(totalExpenses)}</Text>
+      </Text>
+      <Text style={styles.summaryText}>
+        Savings: <Text style={getMoneyTextStyle(savings)}>{formatMoney(savings)}</Text>
+      </Text>
+    </Card>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 15,
+    paddingBottom: 7,
+  },
+  periodTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 15,
+  },
+  summaryText: {
+    fontSize: 18,
+    color: "#333",
+    marginBottom: 10,
+  },
+});
 
 export default Home;
